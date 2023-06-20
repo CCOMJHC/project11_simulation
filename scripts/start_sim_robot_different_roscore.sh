14ba9db31311237f72a8cab907cbb74204ee0e3c
Roland Arsenault <roland@ccom.unh.edu>
2023-06-20 15:33:04 -0400

    Updates to test udp bridge

--- scripts/start_sim_robot_different_roscore.sh
+++ scripts/start_sim_robot_different_roscore.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-tmux new -d -s project11_sim_robot
+tmux new -d -s project11_sim_robot_core
 tmux setenv ROS_MASTER_URI http://localhost:11312
 tmux send-keys "ROS_MASTER_URI=http://localhost:11312 roscore -p 11312" C-m
-tmux splitw -p 90
-tmux send-keys "rosrun rosmon rosmon project11_simulation sim_robot.launch" C-m
-tmux splitw -p 50
-tmux attach -t project11_sim_robot
+
+#tmux new -d -s project11_sim_robot
+#tmux setenv ROS_MASTER_URI http://localhost:11312
+export ROS_MASTER_URI=http://localhost:11312
+rosrun rosmon rosmon project11_simulation sim_robot.launch
+#tmux send-keys "rosrun rosmon rosmon project11_simulation sim_robot.launch" C-m
+#tmux attach -t project11_sim_robot
